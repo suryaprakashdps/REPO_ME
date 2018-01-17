5a4267bf7e3427eba55adf29b5f0878e91d3d0cb
suryaprakashdps <suriyaprakashdps@gmail.com>
2018-01-16 22:47:08 -0600

    reportchanges

--- src/main/java/com/tcs/Repo/Export/ExcelView.java
+++ src/main/java/com/tcs/Repo/Export/ExcelView.java
@@ -28,8 +28,9 @@ import com.tcs.Repo.model.ProjectionVO;
 public class ExcelView extends AbstractXlsView {
 
 	private static final String[] titles = { "Project", "Tower", "ONSITE WON", "OFFSITE WON", "NEAR WON",
-			"OFF LOCATION", "SL1", "SL2", "DIGITAL OFFERING", "PROJECT TYPE", "ON RATE", "OFF RATE", "NS RATE", "JAN",
-			"FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+			"OFF LOCATION", "SL1", "SL2", "DIGITAL OFFERING", "PROJECT TYPE", "ON RATE", "OFF RATE", "NS RATE","Probability",
+			"Q1HC","Q1REV","Q2HC","Q2REV","Q3HC","Q3REV","Q4HC","Q4REV","APR",
+			"MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "JAN", "FEB", "MAR" };
 	private static final String[] subtitles = { "ON HC", "OFF HC", "NEAR HC", "TOTAL HC", "ON REV", "OFF REV",
 			"NEAR REV", "TOTAL REV"
 
@@ -63,20 +64,20 @@ public class ExcelView extends AbstractXlsView {
 		Cell titleCell = titleRow.createCell(0);
 		titleCell.setCellValue("Target Projections");
 		titleCell.setCellStyle(styles.get("title"));
-		sheet.addMergedRegion(CellRangeAddress.valueOf("$A$1:$AA$1"));
+		sheet.addMergedRegion(CellRangeAddress.valueOf("$A$1:$DE$1"));
 
 		// header row
 		Row headerRow = sheet.createRow(1);
 		headerRow.setHeightInPoints(40);
-		int k = 13;
-		int j = 20;
+		int k = 22;
+		int j = 29;
 		int headercellinc = 0;
 		Cell headerCell;
 		for (int i = 0; i < titles.length; i++) {
 			headerCell = headerRow.createCell(headercellinc);
 			headerCell.setCellValue(titles[i]);
 			headerCell.setCellStyle(styles.get("header"));
-			if (i > 12) {
+			if (i > 21) {
 
 				while (headercellinc < j) {
 
@@ -86,6 +87,9 @@ public class ExcelView extends AbstractXlsView {
 					headerCell.setCellValue(titles[i]);
 					headerCell.setCellStyle(styles.get("header"));
 				}
+			
+				CellRangeAddress cellRangeAddress = new CellRangeAddress(1, 1, k, j);
+				sheet.addMergedRegion(cellRangeAddress);
 //				String ref = (char) ('A' + k) + "2:" + (char) ('A' + j) + "2";
 //				sheet.addMergedRegion(CellRangeAddress.valueOf(ref));
 				j = j + 8;
@@ -100,13 +104,18 @@ public class ExcelView extends AbstractXlsView {
 		Row subheaderRow = sheet.createRow(2);
 		subheaderRow.setHeightInPoints(40);
 		Cell subheaderCell;
-		int m = 13;
+		int m = 22;
 		for (int i = 0; i < 12; i++) {
 
 			for (int subtitleArray = 0; subtitleArray < subtitles.length; subtitleArray++) {
 				subheaderCell = subheaderRow.createCell(m);
 				subheaderCell.setCellValue(subtitles[subtitleArray]);
-				subheaderCell.setCellStyle(styles.get("header"));
+				if(i==0 ||i==2||i==4||i==6||i==8||i==10){
+				subheaderCell.setCellStyle(styles.get("subheader"));
+				}
+				if(i==1 ||i==3||i==5||i==7||i==9||i==11){
+					subheaderCell.setCellStyle(styles.get("subheader1"));
+					}
 				m++;
 			}
 
@@ -124,26 +133,67 @@ public class ExcelView extends AbstractXlsView {
 			int cellincrement = 3;
 
 			for (ProjectionVO exportvo : values) {
-
-				System.out.println("Values = " + exportvo.getMonth() + exportvo.getResource_count()
-						+ exportvo.getTower() + exportvo.getRevenue());
-
+				
 				exportvoRow.createCell(1).setCellValue(exportvo.getTower());
+				
+				String month=exportvo.getMonth();
+				 int columnStartValue=0;
+				   int columnEndValue=0;
+				
+				switch(month)
+				{
+			
+				   case "Jan" :
+					   columnStartValue=17;
+					   columnEndValue=17;
+					   
+					   createcell(exportvoRow,columnStartValue,columnEndValue,exportvo);
+				      
+				      break; 
+				   
+				   case "Feb" :
+					   columnStartValue=25;
+					   columnEndValue=25;
+					   
+					   createcell(exportvoRow,columnStartValue,columnEndValue,exportvo);
+				   
+				      break; 
+				
+				   default : 
+					   System.out.println("inside default");
+				   
+				}
 
-				// for (int i = 2; i <= values.size(); i++)
-
-				exportvoRow.createCell(i).setCellValue(exportvo.getMonth());
-				System.out.println("rc is" + exportvo.getResource_count());
-
-				exportvoRow.createCell(cellincrement).setCellValue(exportvo.getRevenue());
-				cellincrement++;
-				cellincrement++;
-				i++;
-				i++;
+//				System.out.println("Values = " + exportvo.getMonth() + exportvo.getResource_count()
+//						+ exportvo.getTower() + exportvo.getRevenue());
+//
+//				
+//
+//				// for (int i = 2; i <= values.size(); i++)
+//
+//				exportvoRow.createCell(i).setCellValue(exportvo.getMonth());
+//				System.out.println("rc is" + exportvo.getResource_count());
+//
+//				exportvoRow.createCell(cellincrement).setCellValue(exportvo.getRevenue());
+//				cellincrement++;
+//				cellincrement++;
+//				i++;
+//				i++;
 
 			}
 		}
 
+	}
+
+	private void createcell(Row exportvoRow,int columnStartValue,int columnEndValue,ProjectionVO exportvo) {
+		
+		for(int i=columnStartValue;i<=columnEndValue;i++){
+			
+			exportvoRow.createCell(i).setCellValue(exportvo.getRevenue());
+			
+		}
+		
+		
 	}
 
 	/**
@@ -172,6 +222,31 @@ public class ExcelView extends AbstractXlsView {
 		style.setFont(monthFont);
 		style.setWrapText(true);
 		styles.put("header", style);
+		
+		Font subheaderFont = wb.createFont();
+		subheaderFont.setFontHeightInPoints((short) 11);
+		subheaderFont.setColor(IndexedColors.WHITE.getIndex());
+		style = wb.createCellStyle();
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		style.setFillForegroundColor(IndexedColors.BROWN.getIndex());
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		style.setFont(monthFont);
+		style.setWrapText(true);
+		styles.put("subheader", style);
+		
+
+		Font subheaderFont1 = wb.createFont();
+		subheaderFont1.setFontHeightInPoints((short) 11);
+		subheaderFont1.setColor(IndexedColors.WHITE.getIndex());
+		style = wb.createCellStyle();
+		style.setAlignment(HorizontalAlignment.CENTER);
+		style.setVerticalAlignment(VerticalAlignment.CENTER);
+		style.setFillForegroundColor(IndexedColors.BLACK.getIndex());
+		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		style.setFont(monthFont);
+		style.setWrapText(true);
+		styles.put("subheader1", style);
 
 		style = wb.createCellStyle();
 		style.setAlignment(HorizontalAlignment.CENTER);
